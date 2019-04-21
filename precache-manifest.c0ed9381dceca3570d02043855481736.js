a0a6b4063497525f65d82ac761991256f6e73425
PrakharPro <prakharrajput112@gmail.com>
2019-04-21 19:13:16 +0400

    Updates

---
+++ precache-manifest.c0ed9381dceca3570d02043855481736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "0d75443ca6fcb367c930",
+    "url": "/robofriends/static/js/main.e6b1245a.chunk.js"
+  },
+  {
+    "revision": "c1af8f612bc115d9e299",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "0d75443ca6fcb367c930",
+    "url": "/robofriends/static/css/main.28e12ae5.chunk.css"
+  },
+  {
+    "revision": "c1af8f612bc115d9e299",
+    "url": "/robofriends/static/css/2.26af0ac5.chunk.css"
+  },
+  {
+    "revision": "c3af27c0e03c5375a2d05219ac282c85",
+    "url": "/robofriends/index.html"
+  }
+];
